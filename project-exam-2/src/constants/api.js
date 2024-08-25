4a99f8823ba94c0800278b183a40c5866102b0a2
Kristoffer Iversen <73189395+KIVERSEN86@users.noreply.github.com>
2024-08-25 19:15:16 +0200

    Added profile url const

--- project-exam-2/src/constants/api.js
+++ project-exam-2/src/constants/api.js
@@ -1,5 +1,5 @@
 export const Base_Url = "https://v2.api.noroff.dev/";
 export const Login_Url = `${Base_Url}auth/login`;
 export const POST_URL = `${Base_Url}social/posts`;
-
-export const API_KEY = "5024ef9a-b1bc-456f-b970-1c2ee2cf7eea";
+export const PROFILE_URL = `${Base_Url}social/profiles`;
+export const API_KEY = "3ff301bb-32ea-4e3e-b63f-f6f48e09af1c";
